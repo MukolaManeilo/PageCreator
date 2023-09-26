b2ce97893b1de1d27e356a6de7f280236d5416b3
KolyaManeilo <kolamanejlo@gmail.com>
2023-09-26 07:43:28 +0300

    Switching to MVC

--- PageCreator/PageCreator/Program.cs
+++ PageCreator/PageCreator/Program.cs
@@ -2,37 +2,4 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
-app.UseStaticFiles();
-
-app.Map("/", async (context) =>
-{
-    if (context.Request.Method == "POST")
-    {
-        var form = context.Request.Form;
-        string name = form["login"]!;
-        string age = form["password"]!;
-        await context.Response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p></div>");
-    }
-    else
-        context.Response.Redirect("/authorizaton");
-
-});
-
-
-app.Map("/authorizaton", async (context) =>
-{
-    context.Response.ContentType = "text/html; charset=utf-8";
-    await context.Response!.SendFileAsync("html/AuthorizationPage.html");
-});
-
-//app.Map("/viewer", () =>
-//{
-
-//});
-
-//app.Map("/editor", () =>
-//{
-
-//});
-
 app.Run();
--- PageCreator/PageCreator/models/OtherParamUser.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PageCreator.models
-{
-    public class OtherParamUser
-    {
-    }
-}
