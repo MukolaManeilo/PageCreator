9752b576fda2d20ee995a385b6c6ddfff5864cb5
KolyaManeilo <kolamanejlo@gmail.com>
2023-10-13 22:58:03 +0300

    Add Users model

---
+++ PageCreator/PageCreator/Models/Users.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PageCreator.Models
+{
+    public class Users
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required] 
+        [EmailAddress]
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Column("password")]
+        public string Password {get; set; }
+
+        [Required]
+        [Column("created_at")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
