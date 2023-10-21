c97b3a8745362216c62c7dffaec2757d3f10d73b
KolyaManeilo <kolamanejlo@gmail.com>
2023-10-21 06:44:14 +0300

    Add Migration, Update DataBase, Connect DataBase, Update Layout

---
+++ PageCreator/PageCreator/Controllers/PagesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PageCreator.Data;
+using PageCreator.Models;
+
+namespace PageCreator.Controllers
+{
+    public class PagesController : Controller
+    {
+        private readonly PageCreatorContext _context;
+
+        public PagesController(PageCreatorContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pages
+        public async Task<IActionResult> Index()
+        {
+            var pageCreatorContext = _context.Pages.Include(p => p.Users);
+            return View(await pageCreatorContext.ToListAsync());
+        }
+
+        // GET: Pages/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Pages == null)
+            {
+                return NotFound();
+            }
+
+            var pages = await _context.Pages
+                .Include(p => p.Users)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pages == null)
+            {
+                return NotFound();
+            }
+
+            return View(pages);
+        }
+
+        // GET: Pages/Create
+        public IActionResult Create()
+        {
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Pages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Access,CreatedBy,CreatedDate,Content")] Pages pages)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pages);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "Id", "Email", pages.CreatedBy);
+            return View(pages);
+        }
+
+        // GET: Pages/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Pages == null)
+            {
+                return NotFound();
+            }
+
+            var pages = await _context.Pages.FindAsync(id);
+            if (pages == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "Id", "Email", pages.CreatedBy);
+            return View(pages);
+        }
+
+        // POST: Pages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Access,CreatedBy,CreatedDate,Content")] Pages pages)
+        {
+            if (id != pages.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pages);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PagesExists(pages.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "Id", "Email", pages.CreatedBy);
+            return View(pages);
+        }
+
+        // GET: Pages/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Pages == null)
+            {
+                return NotFound();
+            }
+
+            var pages = await _context.Pages
+                .Include(p => p.Users)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pages == null)
+            {
+                return NotFound();
+            }
+
+            return View(pages);
+        }
+
+        // POST: Pages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Pages == null)
+            {
+                return Problem("Entity set 'PageCreatorContext.Pages'  is null.");
+            }
+            var pages = await _context.Pages.FindAsync(id);
+            if (pages != null)
+            {
+                _context.Pages.Remove(pages);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PagesExists(int id)
+        {
+          return (_context.Pages?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
